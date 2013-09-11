de1eee93bd85a81386db32e540dc513ab933d51b
shive <shive@users.noreply.github.com>
2013-09-11 22:16:20 +0900

    test001-forwin

---
+++ README.rst
@@ -0,0 +1,16 @@
+﻿.. -*- coding: utf-8-with-signature-unix -*-
+
+====
+概要
+====
+
+    - scons を試してみるリポジトリ
+
+
+----
+内容
+----
+
+    - test001-forwin: Windows環境でVisualStudio2008/2010/2012とMinGWでビルドする設定
+
+
---
+++ test001-forwin/main.cpp
@@ -0,0 +1,14 @@
+﻿// -*- coding: utf-8-with-signature-dos -*-
+//======================================================================================================================
+
+#include "stdafx.h"
+
+int main(int argc, const char** argv) {
+#ifdef _DEBUG
+    ::printf("hoge hoge hoge.\n");
+#else
+    ::printf("HOGE HOGE HOGE.\n");
+#endif
+    return 0;
+}
+
